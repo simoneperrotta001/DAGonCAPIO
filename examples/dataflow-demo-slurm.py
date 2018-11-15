36be6bc00762a016c44b17c535f08448f6b4ff4d
Raffaele Montella <raffaele.montella@uniparthenope.it>
2018-11-15 22:55:55 +0000

    New file

---
+++ examples/dataflow-demo-slurm.py
@@ -0,0 +1,45 @@
+from dagon import Workflow
+from dagon import batch
+import json
+import sys
+import datetime
+import os.path
+
+# Check if this is the main
+if __name__ == '__main__':
+
+  config={
+    "scratch_dir_base":"/tmp/test6",
+    "remove_dir":False
+  }
+
+  # Create the orchestration workflow
+  workflow=Workflow("DataFlow-Demo",config)
+  
+  # The task a
+  taskA=batch.Batch("A","mkdir output;hostname > output/f1.txt")
+  
+  # The task b
+  taskB=batch.Batch("B","echo $RANDOM > f2.txt; cat workflow:///A/output/f1.txt >> f2.txt")
+  
+  # The task c
+  taskC=batch.Batch("C","echo $RANDOM > f2.txt; cat workflow:///A/output/f1.txt >> f2.txt")
+  
+  # The task d
+  taskD=batch.Batch("D","cat workflow:///B/f2.txt > f3.txt; cat workflow:///C/f2.txt >> f3.txt")
+  
+  # add tasks to the workflow
+  workflow.add_task(taskA)
+  workflow.add_task(taskB)
+  workflow.add_task(taskC)
+  workflow.add_task(taskD)
+
+  workflow.make_dependencies()
+
+  jsonWorkflow=workflow.asJson()
+  with open('dataflow-demo-slurm.json', 'w') as outfile:
+    stringWorkflow=json.dumps(jsonWorkflow,sort_keys=True, indent=2)
+    outfile.write(stringWorkflow)
+ 
+  # run the workflow
+  workflow.run()
